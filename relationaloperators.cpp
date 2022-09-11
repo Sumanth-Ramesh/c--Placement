afa89bb9703264009a92dbe93d04a54b229df33d
Sumanth-Ramesh <sumanthramesh5598@gmail.com>
2022-09-11 22:22:09 +0530

    relationaloperators

---
+++ relationaloperators.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+
+int a = 8;
+int b = 4;
+
+bool first = (a=b);
+cout<< first << endl ;
+
+bool second = (a>b);
+cout<< second << endl ;
+
+bool third = ( a< b);
+cout<< third << endl;
+
+bool fourth = ( a!= b);
+cout << fourth << endl;
+
+
+}
