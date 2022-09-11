8dee65fdaac241c8b8649edb1bee6d96deaef871
Sumanth-Ramesh <sumanthramesh5598@gmail.com>
2022-09-11 22:28:21 +0530

    logicaloperators

---
+++ logicaloperators.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main () {
+
+int a = 90 ;
+int b = 100;
+
+bool ass = (90 && 100);
+cout << ass << endl ;
+
+bool ass1 = (90 ||100);
+cout << ass1 << endl ;
+
+
+bool ass2 = ( a != b);
+cout << ass << endl ;
+
+
+}
