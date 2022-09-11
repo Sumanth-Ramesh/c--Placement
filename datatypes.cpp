d9398020ea0888dc7a717e3d4f8bb61c57702dad
Sumanth-Ramesh <sumanthramesh5598@gmail.com>
2022-09-11 21:07:50 +0530

    datatype and variable

---
+++ datatypes.cpp
@@ -0,0 +1,25 @@
+#include<iostream>;
+using namespace std;
+
+int main(){
+
+    int a = 12345 ;
+    cout<<a<<endl;
+
+    char x = 's';
+    cout<<x<<endl;
+
+    float f = 5.6;
+    cout<< f << endl;
+
+    double d = 5.66;
+    cout<< d << endl;
+
+    bool bs = true;
+    cout<< bs << endl ;
+
+    int size = sizeof(a);
+    cout<< "size of a:"<< size<< endl;
+
+
+}
