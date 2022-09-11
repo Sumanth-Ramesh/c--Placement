fcfac6ede542a2a8f0cb29abc73597e86873e4d3
Sumanth-Ramesh <sumanthramesh5598@gmail.com>
2022-09-11 22:08:52 +0530

    operators'/'

---
+++ operators.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+     int a = 2.0/5 ;
+     cout<< a << endl;
+
+    float b = 8.6/2 ;
+    cout<< b << endl ;
+
+    double d = 3.0/1 ;
+    cout << d << endl;
+
+    int x = 2.0/5;
+    cout<< 2.0/5 << endl;
+
+
+}
