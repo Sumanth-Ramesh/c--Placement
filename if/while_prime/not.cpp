d2b82e552b896d7c15b9f672b1b0e61b59aa5960
Sumanth-Ramesh <sumanthramesh5598@gmail.com>
2022-09-13 12:24:38 +0530

    prime/not

---
+++ if/while_prime/not.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+int n;
+cout<<"enter the value of n:"<< endl;
+cin>> n;
+
+int i =2;
+
+while (i<n){
+    if (n%i==0) {
+        cout << "not prime"<< i << endl;
+    }
+    else {
+        cout<< " is prime "<< i << endl;
+    }
+    i = i +1;
+    
+}
+
+
+
+
+}
